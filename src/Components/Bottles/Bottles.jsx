0c743c227f244dcbe9b3d64a236e884ed8dc291f
akmkamrulhasanreyad <akmkamrulhasanreyad@gmail.com>
2023-09-14 01:58:56 +0600

    now time to breack

--- src/Components/Bottles/Bottles.jsx
+++ src/Components/Bottles/Bottles.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import './Bottles.css'
+import { addToLS, getStoredCart } from "../utilities/localstorage";
 
 const Bottles = () => {
     const [bottles, setBottles] = useState([]);
@@ -12,9 +13,30 @@ const Bottles = () => {
             .then(data => setBottles(data));
     }, []);
 
+    // load cart from local storage
+    useEffect(() => {
+        console.log('called the useEffect', bottles.length)
+        if (bottles.length > 0) {
+            const storageCart = getStoredCart();
+            console.log(storageCart, bottles);
+
+            const saveCart = [];
+            for (const id of storageCart) {
+                console.log(id);
+                const bottle = bottles.find(bottle => bottle.id === id);
+                if (bottle) {
+                    saveCart.push(bottle);
+                }
+            }
+            console.log('saveItem', saveCart);
+            setCart(saveCart);
+        }
+    }, [bottles])
+
     const handleAddToCart = bottle => {
         const newCart = [...cart, bottle];
         setCart(newCart);
+        addToLS(bottle.id)
     }
 
     return (
